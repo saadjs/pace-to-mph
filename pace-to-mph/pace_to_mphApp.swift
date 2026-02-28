2e5fc35e0a15973136dd8224bcf309a281bce858
Saad Bash <skbash89@gmail.com>
2026-02-28 16:44:10 -0500

    Initial Commit

---
+++ pace-to-mph/pace_to_mphApp.swift
@@ -0,0 +1,17 @@
+//
+//  pace_to_mphApp.swift
+//  pace-to-mph
+//
+//  Created by Saad Bash on 2/28/26.
+//
+
+import SwiftUI
+
+@main
+struct pace_to_mphApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ pace-to-mphTests/pace_to_mphTests.swift
@@ -0,0 +1,17 @@
+//
+//  pace_to_mphTests.swift
+//  pace-to-mphTests
+//
+//  Created by Saad Bash on 2/28/26.
+//
+
+import Testing
+@testable import pace_to_mph
+
+struct pace_to_mphTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
